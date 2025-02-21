3430e3b234db3f3f38745a9b860a0d435f8d2955
Jace Webster <jacewebster@Jaces-Laptop.lan>
2025-02-21 11:37:14 -0600

    plotting

--- R/plotting.R
+++ R/plotting.R
@@ -5,19 +5,13 @@
 #' @param obj A SCCNVisObject made using createPlotObject()
 #' @param gr A GenomicRanges:GRanges object of the regions to graph. If NULL, all regions will be plotted. Default = NULL
 #' @param add.noise Add minor noise to the data for clustering purposes. Default = TRUE
-#' @param min.color.value Minimum value in the color scale. Default = 0
-#' @param med.color.value Medium value in the color scale. Default = 1
-#' @param max.color.value Max value in the color scale. Default = 2
-#' @param color.ramp A color ramp generated using circlize::colorRamp2. If provided, the min/med/max.color.values will be ignored. Default = NULL
+#' @param color.ramp A color ramp generated using circlize::colorRamp2. If NULL, a red/white/blue color ramp will be used, centered on the median. Default = NULL
 #' @param verbose Verbose logging. Default = FALSE
 #' @return Returns a ComplexHeatmap object
 #' @export
 makeSCHeatmap <- function(obj,
                           gr = NULL,
                           add.noise = T,
-                          min.color.value = 0,
-                          med.color.value = 1,
-                          max.color.value = 2,
                           color.map = NULL,
                           verbose = F) {
   .validateObject(obj)
@@ -62,9 +56,11 @@ makeSCHeatmap <- function(obj,
   if(is.null(color.map)) {
     .verboseLog(verbose,"Using default color scheme")
 
+    quantiles <- quantile(plot.data, probs = c(0.01, 0.99))
+
     cl <- c("Blue","White","Red")
     color.map <- circlize::colorRamp2(
-      c(min.color.value, med.color.value, max.color.value),
+      c(quantiles[1], median(plot.data), quantiles[2]),
       cl
     )
   }
@@ -108,5 +104,66 @@ makeSCHeatmap <- function(obj,
   )
 
   ht
+
+}
+
+
+#' Make a line plot showing the average score across different regions
+#'
+#' @param obj SCCNVis object
+#' @param gr A GenomicRanges::GRanges object specifying which reasons to plot. If NULL, all regions will be plotted. Default = NULL
+#' @param window.size Window size for moving average calculation
+#' @param clusters Character vector specifying clusters for cells. If provided, should be same as the number of cells plotted. Default = NULL
+#' @param verbose Verbose logging. Default = FALSE
+#' @return Returns a ggplot2::gg object
+#' @export
+makeLinePlot <- function(obj,
+                         gr = NULL,
+                         window.size = 5,
+                         clusters = NULL,
+                         verbose = F) {
+
+  .validateObject(obj)
+
+  .verboseLog(verbose, "Running makeLinePlot()")
+
+  plot.data <- obj@Matrix
+  plot.granges <- obj@GRanges
+  if(!is.null(gr)) {
+    .verboseLog(verbose, "Subsetting to desired regions")
+
+    overlap <- GenomicRanges::findOverlaps(plot.granges, gr)
+    plot.granges <- plot.granges[overlap@from]
+    if(length(plot.granges) < 1) {
+      stop("Provided GRanges object does not overlap with any regions in the SCCNVisObject")
+    }
+    plot.granges <- .standardizeGRanges(plot.granges)
+    plot.data <- plot.data[,plot.granges$index]
+    .verboseLog(verbose, paste0("Genomic bins remaining: ", ncol(plot.data)))
+  }
+
+  #Calculating column means
+  col.means <- data.frame(rn = c(1:ncol(plot.data)), Mean = colMeans(plot.data))
+
+  #Rolling means
+  .verboseLog(verbose, paste0("Calculating rolling means, with window size:", window.size))
+  moving.average <- .movingAverage(col.means, data.frame(plot.granges), window.size = window.size)
+
+  #Combine rolling means and granges
+  mgr <- merge(moving.average,data.frame(plot.granges), by="index", all.y = T)
+  #Something this merge causes some NA values for some reason
+  mgr[mgr$index %in% moving.average$index,"moving.average"] <- mgr$moving.average
+
+  p <- ggplot2::ggplot(mgr) +
+        ggplot2::geom_line(ggplot2::aes(x=index, y=moving.average)) +
+        ggplot2::facet_grid(~seqnames, scales = 'free_x', space = 'free_x', switch = 'x') +
+        ggplot2::theme_classic() +
+        ggplot2::labs(x="",y="") +
+        ggplot2::theme(axis.text.x = ggplot2::element_blank(),
+                   axis.ticks.x = ggplot2::element_blank(),
+                   panel.margin = ggplot2::unit(0, "lines")) +
+        ggplot2::scale_x_continuous(expand = c(0.1, 0.1))
+
+  p
 
 }
